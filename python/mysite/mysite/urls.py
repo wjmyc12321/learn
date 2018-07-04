56af33fe2c7cb4ddfb6191f79d0cdd718ea8361f
wjmyc12321 <984976016@qq.com>
2018-07-04 17:31:33 +0800

    由60.12更新

--- python/mysite/mysite/urls.py
+++ python/mysite/mysite/urls.py
@@ -16,6 +16,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from learn import views as learn_views # new
 urlpatterns = [
-    url(r'^add/$',learn_views.add,name='add'), # new
+    url(r'^add/$',learn_views.add,name='add'),
+    url(r'^add/(\d+)/(\d+)/$',learn_views.add2,name='add2') ,
     url(r'^admin/', admin.site.urls),
     ]

acc2d7b684ed69ca90483a9bf1ebf825478aa2e1
wjmyc12321 <984976016@qq.com>
2018-07-04 18:15:57 +0800

    12

--- python/mysite/mysite/urls.py
+++ python/mysite/mysite/urls.py
@@ -16,7 +16,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from learn import views as learn_views # new
 urlpatterns = [
+    url(r'^$',learn_views.index,name='home'),
     url(r'^add/$',learn_views.add,name='add'),
-    url(r'^add/(\d+)/(\d+)/$',learn_views.add2,name='add2') ,
+    url(r'^add/(\d+)/(\d+)/$',learn_views.old_add2_redirect),
+    url(r'^new_add/(\d+)/(\d+)/$',learn_views.add2,name='add2') ,
     url(r'^admin/', admin.site.urls),
     ]

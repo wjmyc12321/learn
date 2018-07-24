0082c1a67d2f1f930579fd9fc8ad08989b797109
wjmyc12321 <984976016@qq.com>
2018-07-24 18:39:46 +0800

    由 Cloud Studio 更新，https://studio.coding.net

--- python/mysite/learn/views.py
+++ python/mysite/learn/views.py
@@ -6,9 +6,18 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
 
-def add(request):
-    a = request.GET['a']
-    b = request.GET['b']
+def index(request):
+    return render(request,'index.html')
+
+def home(request):
+    my_dict = {'site':u'自强学堂','content':u'各种IT技术'}
+    return render(request,'home.html',{'List1':my_dict})
+
+
+def add(request,a,b):
     c = int(a) + int(b)
     return HttpResponse(str(c))

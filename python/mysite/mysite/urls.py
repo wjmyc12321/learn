5e3e0ecd863504b174ddd1e6c4e5dad855409b46
wjmyc12321 <984976016@qq.com>
2018-07-24 18:39:09 +0800

    由 Cloud Studio 更新，https://studio.coding.net

--- python/mysite/mysite/urls.py
+++ python/mysite/mysite/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from learn import views as learn_views # new
+
 urlpatterns = [
-    url(r'^$',learn_views.index,name='home'),
-    url(r'^add/$',learn_views.add,name='add'),
-    url(r'^add/(\d+)/(\d+)/$',learn_views.old_add2_redirect),
-    url(r'^new_add/(\d+)/(\d+)/$',learn_views.add2,name='add2') ,
+    url(r'^$',learn_views('index'),name='index'),
     url(r'^admin/', admin.site.urls),
     ]
